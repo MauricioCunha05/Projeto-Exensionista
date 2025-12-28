3aa7f4faa13eaf87ebaa5e7e1ac7a10e6cbc9ba1
Maurício <jecamcusack@gmail.com>
2025-12-28 17:52:29 -0300

    Basic event table created

--- src/index.php
+++ src/index.php
@@ -1,3 +1,7 @@
+<?php
+require("Classes/Evento.php")
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
--- src/views/eventos.php
+++ src/views/eventos.php
@@ -1,1 +1,21 @@
-Eventos
+<?php
+$Eventos = new Evento("data/eventos.json");
+$eventos_array = $Eventos->all()['eventos'];
+?>
+
+<table id="Eventos">
+    <tr>
+        <th>Data</th>
+        <th>Evento</th>
+        <th>Descrição</th>
+    </tr>
+    <?php 
+    foreach ($eventos_array as $evento) {
+        echo "<tr>";
+        echo "<td>".$evento["data"]."</td>";
+        echo "<td>".$evento["titulo"]."</td>";
+        echo "<td>".$evento["descricao"]."</td>";
+        echo "</tr>";
+    }
+    ?>
+</table>
